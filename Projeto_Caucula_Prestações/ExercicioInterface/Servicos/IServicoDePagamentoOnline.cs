78b8e74b094b7d55c21fb5087e9391eb3266d9b9
WinilsondePaula <magraofulero@gmail.com>
2020-06-26 16:13:39 -0300

    Esboço caucula prestações

---
+++ Projeto_Caucula_Prestações/ExercicioInterface/Servicos/IServicoDePagamentoOnline.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ExercicioInterface.Servicos {
+    interface IServicoDePagamentoOnline {
+
+        double Valor_Original(double montante);
+        double Juros(double montante, int meses);
+    }
+}

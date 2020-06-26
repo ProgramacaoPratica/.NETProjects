0e7f990f76f4fbebbb1462cb0a6f4d679aa1c515
WinilsondePaula <magraofulero@gmail.com>
2020-06-26 16:17:04 -0300

    atualizando repositorio

--- Projeto_Caucula_Prestações/ExercicioInterface/Servicos/IServicoDePagamentoOnline.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace ExercicioInterface.Servicos {
-    interface IServicoDePagamentoOnline {
-
-        double Valor_Original(double montante);
-        double Juros(double montante, int meses);
-    }
-}
